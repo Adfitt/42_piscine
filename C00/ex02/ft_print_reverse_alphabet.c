68d8b441066735fd4528a49b6e4446facf06788a
Adfitt <adrianfittipaldi4@gmail.com>
2026-08-31 19:26:09 +0200

    First project

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrifitt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/03 11:57:25 by adrifitt          #+#    #+#             */
+/*   Updated: 2026/03/03 12:28:11 by adrifitt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+/*
+int     main(void)
+{
+        ft_print_reverse_alphabet();
+        write(1, "\n", 1);
+        return (0);
+}
+*/
